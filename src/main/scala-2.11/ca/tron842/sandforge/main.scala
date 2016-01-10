691a6d86b3a1be0f1c5f24c747efa483ef1b42d6
Tron842 <brodyatto@gmail.com>
2016-01-09 21:20:05 -0500

    Added Main and MainSpec

---
+++ src/main/scala-2.11/ca/tron842/sandforge/main.scala
@@ -0,0 +1,10 @@
+package ca.tron842.sandforge
+
+/**
+  * Created by Tron482 on 1/9/2016.
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
---
+++ src/test/scala-2.11/ca/tron842/sandforge/MainSpec.scala
@@ -0,0 +1,13 @@
+package ca.tron842.sandforge
+
+/**
+  * Created by Tron842 on 1/9/2016.
+  */
+import org.specs2.Specification
+
+
+
+//@RunWith(classOf[JUnitRunner])
+class MainSpec extends Specification { def is = """"""
+
+}
